0f87ce103e04d788e3231285d423855a8470c88e
rpprppprppr <kondrenkodo@gmail.com>
2025-12-25 04:51:20 +0500

    add auth for likes (post and comment)

--- src/Blog/Http/Actions/CommentLikes/AddLikeToComment.php
+++ src/Blog/Http/Actions/CommentLikes/AddLikeToComment.php
@@ -4,43 +4,49 @@ namespace src\Blog\Http\Actions\CommentLikes;
 
 use src\Blog\CommentLike;
 use src\Blog\UUID;
-use src\Blog\Http\Actions\ActionsInterface;
-use src\Blog\Http\Request;
-use src\Blog\Http\Response;
-use src\Blog\Http\ErrorResponse;
-use src\Blog\Http\SuccessfulResponse;
 
-use src\Blog\Repositories\CommentLikesRepository\CommentLikeRepositoryInterface;
-use src\Blog\Repositories\CommentsRepository\CommentRepositoryInterface;
-use src\Blog\Repositories\UsersRepository\UserRepositoryInterface;
+use src\Blog\Http\Auth\TokenAuthenticationInterface;
 
+use src\Blog\Exceptions\AuthException;
 use src\Blog\Exceptions\HttpException;
 use src\Blog\Exceptions\CommentNotFoundException;
 use src\Blog\Exceptions\UserNotFoundException;
+
+use src\Blog\Repositories\CommentLikesRepository\CommentLikeRepositoryInterface;
+
+use src\Blog\Http\Actions\ActionsInterface;
+use src\Blog\Http\ErrorResponse;
+use src\Blog\Http\SuccessfulResponse;
+use src\Blog\Http\Request;
+use src\Blog\Http\Response;
 
 readonly class AddLikeToComment implements ActionsInterface
 {
     public function __construct(
         private CommentLikeRepositoryInterface $commentLikeRepository,
-        private CommentRepositoryInterface $commentRepository,
-        private UserRepositoryInterface $userRepository
-    ) {}
+        private TokenAuthenticationInterface $authentication
+    )
+    {}
 
     public function handle(Request $request): Response
     {
         try {
-            $commentUuid = new UUID($request->jsonBodyField('comment_uuid'));
-            $userUuid = new UUID($request->jsonBodyField('user_uuid'));
+            $user = $this->authentication->user($request);
+        } catch (AuthException $error) {
+            return new ErrorResponse($error->getMessage());
+        }
 
-            $this->commentRepository->get($commentUuid);
-            $this->userRepository->get($userUuid);
+        try {
+            $newCommentLikeUuid = UUID::random();
+            $commentUuid = new UUID($request->jsonBodyField('comment_uuid'));
+            $userUuid = $user->getId();
 
             if ($this->commentLikeRepository->hasLike($commentUuid, $userUuid)) {
                 return new ErrorResponse("User already liked this comment");
             }
 
             $commentLike = new CommentLike(
-                UUID::random(),
+                $newCommentLikeUuid,
                 $commentUuid,
                 $userUuid
             );
@@ -53,7 +59,7 @@ readonly class AddLikeToComment implements ActionsInterface
         $this->commentLikeRepository->save($commentLike);
 
         return new SuccessfulResponse([
-            'uuid' => (string)$commentLike->getId()
+            'uuid' => (string)$newCommentLikeUuid
         ]);
     }
 }
--- src/Blog/Http/Actions/PostLikes/AddLikeToPost.php
+++ src/Blog/Http/Actions/PostLikes/AddLikeToPost.php
@@ -4,43 +4,49 @@ namespace src\Blog\Http\Actions\PostLikes;
 
 use src\Blog\PostLike;
 use src\Blog\UUID;
-use src\Blog\Http\Actions\ActionsInterface;
-use src\Blog\Http\Request;
-use src\Blog\Http\Response;
-use src\Blog\Http\ErrorResponse;
-use src\Blog\Http\SuccessfulResponse;
 
-use src\Blog\Repositories\PostLikesRepository\PostLikeRepositoryInterface;
-use src\Blog\Repositories\PostsRepository\PostRepositoryInterface;
-use src\Blog\Repositories\UsersRepository\UserRepositoryInterface;
+use src\Blog\Http\Auth\TokenAuthenticationInterface;
 
+use src\Blog\Exceptions\AuthException;
 use src\Blog\Exceptions\HttpException;
 use src\Blog\Exceptions\PostNotFoundException;
 use src\Blog\Exceptions\UserNotFoundException;
+
+use src\Blog\Repositories\PostLikesRepository\PostLikeRepositoryInterface;
+
+use src\Blog\Http\Actions\ActionsInterface;
+use src\Blog\Http\ErrorResponse;
+use src\Blog\Http\SuccessfulResponse;
+use src\Blog\Http\Request;
+use src\Blog\Http\Response;
 
 readonly class AddLikeToPost implements ActionsInterface
 {
     public function __construct(
         private PostLikeRepositoryInterface $postLikeRepository,
-        private PostRepositoryInterface $postRepository,
-        private UserRepositoryInterface $userRepository
-    ) {}
+        private TokenAuthenticationInterface $authentication
+    )
+    {}
 
     public function handle(Request $request): Response
     {
         try {
-            $postUuid = new UUID($request->jsonBodyField('post_uuid'));
-            $userUuid = new UUID($request->jsonBodyField('user_uuid'));
+            $user = $this->authentication->user($request);
+        } catch (AuthException $error) {
+            return new ErrorResponse($error->getMessage());
+        }
 
-            $this->postRepository->get($postUuid);
-            $this->userRepository->get($userUuid);
+        try {
+            $newPostLikeUuid = UUID::random();
+            $postUuid = new UUID($request->jsonBodyField('post_uuid'));
+            $userUuid = $user->getId();
 
             if ($this->postLikeRepository->hasLike($postUuid, $userUuid)) {
                 return new ErrorResponse("User already liked this post");
             }
 
             $postLike = new PostLike(
-                UUID::random(),
+                $newPostLikeUuid,
                 $postUuid,
                 $userUuid
             );
